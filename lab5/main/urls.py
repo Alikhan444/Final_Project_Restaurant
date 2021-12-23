ac9352b5d6e0dcb916aad28422d86f2bd5768011
Alikhan444 <96569942+Alikhan444@users.noreply.github.com>
2021-12-23 15:19:32 +0600

    Final_Project
    Final Project Restaurant Alikhan and Akzhol

---
+++ lab5/main/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name ='about'),
+    path('menu', views.menu, name ='menu'),
+    path('Payment', views.pay, name ='pay'),
+    path('information_about_project', views.info, name ='info'),
+    path('information_about_teammates', views.team, name ='team'),
+    path('developers_contacts', views.contacts, name ='contacts'),
+
+    path('/create', views.create, name='create'),
+    path('/<int:pk>', views.DishesDetailView.as_view(), name='detail'),
+    path('/<int:pk>/update', views.DishesUpdateView.as_view(), name='update'),
+    path('/<int:pk>/delete', views.DishesDeleteView.as_view(), name='delete')
+]
